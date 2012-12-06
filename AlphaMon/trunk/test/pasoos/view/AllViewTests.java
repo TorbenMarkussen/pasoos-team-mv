7a9e26b50e63c40e162d737c721d99b18a7759ff
torben@granbakken3.dk <torben@granbakken3.dk@0d074906-a24a-257d-28a7-d112fce6c4fc>
2012-12-06 20:30:28 +0000

    Failing unit test fixed

--- AlphaMon/trunk/test/pasoos/view/AllViewTests.java
+++ AlphaMon/trunk/test/pasoos/view/AllViewTests.java
@@ -6,7 +6,11 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+        AIPlayerStateTest.class,
+        GammonBuildDirectorTest.class,
         HotgammonPieceFactoryTest.class,
+        GameEventDecoratorTest.class,
+        //GerryPlayerTest.class,
         HotgammonPositioningStrategyTest.class})
 public class AllViewTests {
 
