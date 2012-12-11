219e6114bc2cdc8987842d97900b8e53874f4619
tjip@pasma.dk <tjip@pasma.dk@0d074906-a24a-257d-28a7-d112fce6c4fc>
2012-12-11 18:04:34 +0000

    simplified initial state

--- AlphaMon/trunk/src/pasoos/hotgammon/gamestatemachine/Initial.java
+++ AlphaMon/trunk/src/pasoos/hotgammon/gamestatemachine/Initial.java
@@ -6,17 +6,11 @@ import pasoos.hotgammon.Game;
 import pasoos.hotgammon.Location;
 import pasoos.hotgammon.obsolete.minidraw_controller.GammonMove;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Initial extends BaseState {
-    private List<GammonMove> moves;
-    private List<GammonMove> processedMoves;
+    private int animationCount;
 
     public Initial(StateId stateId) {
         super(stateId);
-        moves = new ArrayList<GammonMove>();
-        processedMoves = new ArrayList<GammonMove>();
     }
 
     @Override
@@ -42,50 +36,36 @@ public class Initial extends BaseState {
         animateRack();
     }
 
-    private void processRackMoves() {
-        if (moves.size() > 0) {
-            GammonMove move = moves.remove(0);
-            startAnimation(move);
-            processedMoves.add(move);
-        }
-    }
-
     private void animateRack() {
         Game game = context.getGame();
+
         for (Location loc : Location.values()) {
             int count = game.getCount(loc);
             Color color = game.getColor(loc);
 
             for (int i = 0; i < count; i++) {
-                if (color == Color.BLACK)
-                    moves.add(new GammonMove(Location.B_BEAR_OFF, loc));
-                else
-                    moves.add(new GammonMove(Location.R_BEAR_OFF, loc));
+                Location from = color == Color.BLACK ? Location.B_BEAR_OFF : Location.R_BEAR_OFF;
+                startAnimation(new GammonMove(from, loc));
             }
         }
-        processRackMoves();
     }
 
     private void startAnimation(final GammonMove move) {
+        animationCount++;
         context.moveAnimated(
                 move.getFrom(),
                 move.getTo(),
                 new NullAnimationCallback<Location>() {
                     @Override
-                    public void afterStart(Location from, Location to) {
-                        processRackMoves();
-                    }
-
-                    @Override
                     public void afterEnd(Location from, Location to) {
-                        startGameAfterRack();
+                        animationEnded();
                     }
                 });
     }
 
-    private void startGameAfterRack() {
-        processedMoves.remove(0);
-        if (processedMoves.size() == 0) {
+    private void animationEnded() {
+        animationCount--;
+        if (animationCount == 0) {
             context.getGame().newGame();
             context.rollDice();
         }
