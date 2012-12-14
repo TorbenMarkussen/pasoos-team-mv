a2840be8823f225ef2e22755a084d2e248421061
tjip@pasma.dk <tjip@pasma.dk@0d074906-a24a-257d-28a7-d112fce6c4fc>
2012-12-14 08:07:37 +0000

    removed magic constant

--- AlphaMon/trunk/src/pasoos/hotgammon/animatedgame/ui/MoveAnimationConfiguratorStrategyImpl.java
+++ AlphaMon/trunk/src/pasoos/hotgammon/animatedgame/ui/MoveAnimationConfiguratorStrategyImpl.java
@@ -5,32 +5,35 @@ import minidraw.animatedboard.MoveAnimationConfiguratorStrategy;
 import minidraw.animation.easings.BezierMovement;
 import minidraw.animation.easings.EasingFunctionStrategy;
 import minidraw.animation.TimeInterval;
+import pasoos.hotgammon.boardphysics.BoardPhysics;
 
 import java.awt.*;
 
 public class MoveAnimationConfiguratorStrategyImpl implements MoveAnimationConfiguratorStrategy {
 
+    Rectangle board = BoardPhysics.board();
+
     @Override
     public void configureAnimation(Point from, Point to, AnimationConfiguration cfg) {
-        cfg.setTimeline(getTimeInterval(from, to));
+        cfg.setTimeline(createTimeInterval(from, to));
         cfg.setEasingFuntion(createEasingFunction(from, to));
     }
 
     private EasingFunctionStrategy createEasingFunction(Point from, Point to) {
-        return new BezierMovement(getWayPoint(from), getWayPoint(to));
+        return new BezierMovement(createWayPoint(from), createWayPoint(to));
     }
 
-    private Point getWayPoint(Point point) {
-        return new Point(point.x, 220);
+    private Point createWayPoint(Point point) {
+        return new Point(point.x, board.height / 2);
     }
 
-    private TimeInterval getTimeInterval(Point from, Point to) {
+    private TimeInterval createTimeInterval(Point from, Point to) {
         return TimeInterval.fromNow().duration(calculateIntervalDuration(from, to));
     }
 
     private int calculateIntervalDuration(Point from, Point to) {
         int deltaX = Math.abs(from.x - to.x);
-        int deltaY = Math.max(Math.abs(from.y - to.y), 220);
+        int deltaY = Math.max(Math.abs(from.y - to.y), board.height / 2);
         int hypotenuse = (int) Math.sqrt(Math.pow(deltaX, 2) + Math.pow(deltaY, 2));
         return (hypotenuse * 3);
     }
