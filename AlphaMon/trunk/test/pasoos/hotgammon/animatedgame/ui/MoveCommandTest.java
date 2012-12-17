feee14ed28b363eccb1e7cff4595c991914a9a6d
steffenlpoulsen@gmail.com <steffenlpoulsen@gmail.com@0d074906-a24a-257d-28a7-d112fce6c4fc>
2012-12-17 22:22:23 +0000

    AddedTest for MoveCommand

---
+++ AlphaMon/trunk/test/pasoos/hotgammon/animatedgame/ui/MoveCommandTest.java
@@ -0,0 +1,23 @@
+package pasoos.hotgammon.animatedgame.ui;
+
+import minidraw.boardgame.Command;
+import org.junit.Test;
+import pasoos.hotgammon.Location;
+import pasoos.hotgammon.animatedgame.GammonStateMachine;
+
+import static org.mockito.Mockito.*;
+
+public class MoveCommandTest {
+
+    @Test
+    public void should_invoke_move_request_on_game_state() {
+        GammonStateMachine state = mock(GammonStateMachine.class);
+        Command moveCommand = new MoveCommand(state);
+        moveCommand.setFromCoordinates(501, 21);
+        moveCommand.setToCoordinates(461, 21);
+        moveCommand.execute();
+
+        verify(state, times(1)).moveRequest(Location.B1, Location.B2);
+    }
+
+}
