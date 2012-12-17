18cccd16caa753b8b1bdbff1b3f449abb0bbdd47
steffenlpoulsen@gmail.com <steffenlpoulsen@gmail.com@0d074906-a24a-257d-28a7-d112fce6c4fc>
2012-12-17 22:06:19 +0000

    AddedTest for SimpleWinnerStrategy

--- AlphaMon/trunk/test/pasoos/hotgammon/rules/winner/SimpleWinnerStrategyTest.java
+++ AlphaMon/trunk/test/pasoos/hotgammon/rules/winner/SimpleWinnerStrategyTest.java
@@ -10,33 +10,28 @@ import pasoos.hotgammon.rules.WinnerStrategy;
 import static junit.framework.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static pasoos.hotgammon.Color.BLACK;
 
-public class BackgammonWinnerStrategyTest {
+public class SimpleWinnerStrategyTest {
 
     WinnerStrategy winnerStrategy;
     GameState state;
 
     @Before
     public void setup() {
-        winnerStrategy = new BackgammonWinnerStrategy();
+        winnerStrategy = new SimpleWinnerStrategy();
         state = mock(GameState.class);
     }
 
     @Test
     public void should_have_none_as_winner() {
+        when(state.getTurnCount()).thenReturn(5);
         assertEquals(Color.NONE, winnerStrategy.determineWinner(state));
     }
 
     @Test
-    public void should_have_black_as_winner() {
-        when(state.allCheckersOnBearOff(BLACK)).thenReturn(true);
-        assertEquals(Color.BLACK, winnerStrategy.determineWinner(state));
-    }
+    public void should_have_red_as_winner_after_6_turns() {
+        when(state.getTurnCount()).thenReturn(6);
 
-    @Test
-    public void should_have_red_as_winner() {
-        when(state.allCheckersOnBearOff(Color.RED)).thenReturn(true);
         assertEquals(Color.RED, winnerStrategy.determineWinner(state));
     }
 }
